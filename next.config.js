5ff8452a5e6fae6f12f2fa17cd111b045eb7c147
Atul Tripathi <atul.tripathi@riskcovry.com>
2022-09-28 01:14:39 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
+  },  
 }
 
 module.exports = nextConfig
