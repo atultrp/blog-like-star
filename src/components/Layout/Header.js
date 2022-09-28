39e6e6a33142cc638ac6a6cebde4a21d7a964c98
Atul Tripathi <atul.tripathi@riskcovry.com>
2022-09-29 00:08:20 +0530

    header sticky for desktop view

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,28 +9,32 @@ const Header = () => {
   const [modal, setModal] = useState(false);
   return (
     <>
-      <div className=" justify-between px-16 py-6 shadow-md items-center hidden md:flex">
-        <h1 className="text-4xl font-semibold tracking-wide uppercase custom-font text-transparent bg-clip-text bg-gradient-to-tr from-green-300 via-blue-500 to-green-300">
-          <Link href={"/"} >Blog Like Star </Link></h1>
+      <nav className=" justify-between px-16 py-6 shadow-md items-center hidden md:flex bg-white sticky top-0 z-50">
+        <Link href={"/"} >
+          <h1 className="text-4xl font-semibold tracking-wide uppercase custom-font text-transparent bg-clip-text bg-gradient-to-tr from-green-300 via-blue-500 to-green-300 cursor-pointer hover:scale-105 duration-200">
+            Blog Like Star </h1>
+        </Link>
         <ul className="flex space-x-16 text-base font-semibold items-center">
-          <li className={`${activeTab === 0 ? "opacity-100 text-rose-500" : "opacity-70 hover:text-rose-500"}`} onClick={() => setActiveTab(0)}>
-            <Link href={"/"} >Home</Link>
-          </li>
-          <li className={`${activeTab === 1 ? "opacity-100 text-rose-500" : "opacity-70 hover:text-rose-500"}`} onClick={() => setActiveTab(1)}>
-            <Link href={"/blog"} >Blog</Link>
-          </li>
-          <li className={`${activeTab === 2 ? "opacity-100 text-rose-500" : "opacity-70 hover:text-rose-500"}`} onClick={() => setActiveTab(2)}>
-            <Link href={""} >About</Link>
-          </li>
-          <li className={`${activeTab === 3 ? "opacity-100 text-rose-500" : "opacity-70 hover:text-rose-500"}`} onClick={() => setActiveTab(3)}>
-            <Link href={""} >Contact</Link>
-          </li>
+
+          <Link href={"/"} >
+            <li className={`${activeTab === 0 ? "opacity-100 text-rose-500 cursor-pointer" : "opacity-70 hover:text-rose-500 hover:scale-110 ease-in-out duration-200 cursor-pointer"}`} onClick={() => setActiveTab(0)}>Home</li>
+          </Link>
+          <Link href={"/"} >
+            <li className={`${activeTab === 1 ? "opacity-100 text-rose-500 cursor-pointer" : "opacity-70 hover:text-rose-500 hover:scale-110 ease-in-out duration-200 cursor-pointer"}`} onClick={() => setActiveTab(1)}>Blog</li>
+          </Link>
+          <Link href={"/"} >
+            <li className={`${activeTab === 2 ? "opacity-100 text-rose-500 cursor-pointer" : "opacity-70 hover:text-rose-500 hover:scale-110 ease-in-out duration-200 cursor-pointer"}`} onClick={() => setActiveTab(2)}>About</li>
+          </Link>
+          <Link href={"/"} >
+            <li className={`${activeTab === 3 ? "opacity-100 text-rose-500 cursor-pointer" : "opacity-70 hover:text-rose-500 hover:scale-110 ease-in-out duration-200 cursor-pointer"}`} onClick={() => setActiveTab(3)}>Contact</li>
+          </Link>
           {/* Blog button */}
-          <div className="bg-white text-[#1b1b1b] p-2 rounded-full text-xl cursor-pointer">
-            <Link href={"/createBlog"} ><FaPenNib /></Link>
-          </div>
+          <Link href={"/createBlog"} >
+            <div className="bg-rose-500 text-white p-2 rounded-full text-xl cursor-pointer hover:scale-110 ease-in-out duration-200"><FaPenNib /></div>
+          </Link>
+
         </ul>
-      </div>
+      </nav>
 
       <nav className="md:hidden py-3 px-4 shadow-md sticky top-0 z-50 items-center bg-white">
         <div className=" flex justify-between items-center">
@@ -38,19 +42,22 @@ const Header = () => {
           <HiMenuAlt1 className="text-4xl text-rose-500" onClick={() => { modal ? setModal(false) : setModal(true) }} />
         </div>
         <ul className={`${modal ? 'block' : 'hidden'} absolute right-2 px-4 py-4 shadow-md rounded bg-white font-semibold`}>
-          <li className={`${activeTab === 0 ? "opacity-100 text-rose-500" : "opacity-70 hover:text-rose-500"}`} onClick={() => setActiveTab(0)}>
-            <Link href={"/"} >Home</Link>
-          </li>
-          <li className={`${activeTab === 1 ? "opacity-100 text-rose-500" : "opacity-70 hover:text-rose-500"}`} onClick={() => setActiveTab(1)}>
-            <Link href={"/"} >Blog</Link>
-          </li>
-          <li className={`${activeTab === 2 ? "opacity-100 text-rose-500" : "opacity-70 hover:text-rose-500"}`} onClick={() => setActiveTab(2)}>
-            <Link href={"/"} >About</Link>
-          </li>
-          <li className={`${activeTab === 3 ? "opacity-100 text-rose-500" : "opacity-70 hover:text-rose-500"}`} onClick={() => setActiveTab(3)}>
-            <Link href={"/"} >Contact</Link>
-          </li>
+          <Link href={"/"} >
+            <li className={`${activeTab === 0 ? "opacity-100 text-rose-500 cursor-pointer" : "opacity-70 hover:text-rose-500 hover:scale-110 ease-in-out duration-200 cursor-pointer"}`} onClick={() => setActiveTab(0)}>Home</li>
+          </Link>
+          <Link href={"/"} >
+            <li className={`${activeTab === 1 ? "opacity-100 text-rose-500 cursor-pointer" : "opacity-70 hover:text-rose-500 hover:scale-110 ease-in-out duration-200 cursor-pointer"}`} onClick={() => setActiveTab(1)}>Blog</li>
+          </Link>
+          <Link href={"/"} >
+            <li className={`${activeTab === 2 ? "opacity-100 text-rose-500 cursor-pointer" : "opacity-70 hover:text-rose-500 hover:scale-110 ease-in-out duration-200 cursor-pointer"}`} onClick={() => setActiveTab(2)}>About</li>
+          </Link>
+          <Link href={"/"} >
+            <li className={`${activeTab === 3 ? "opacity-100 text-rose-500 cursor-pointer" : "opacity-70 hover:text-rose-500 hover:scale-110 ease-in-out duration-200 cursor-pointer"}`} onClick={() => setActiveTab(3)}>Contact</li>
+          </Link>
         </ul>
+        <Link href={"/createBlog"}>
+          <div className="bg-rose-500 text-white p-2 rounded-full text-xl cursor-pointer hover:scale-110 ease-in-out duration-200 fixed bottom-2 right-2"><FaPenNib /></div>
+        </Link>
       </nav>
     </>
   )
